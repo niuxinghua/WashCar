3efee7a0a42b4c5dc6f16aa5cfb3f3e3606c4381
niuxinghua <970626879@qq.com>
2015-06-11 11:09:09 +0800

    Initial Commit

---
+++ WashCar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WashCar
+//
+//  Created by niuxinghua on 15/6/11.
+//  Copyright (c) 2015年 Hjojo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
